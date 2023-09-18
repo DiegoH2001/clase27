bc7bedf6b7de13c4f748f49880ac74cf3c93fbdc
DiegoH2001 <diego20hidalgo20@gmail.com>
2023-09-18 10:40:12 -0300

    a

---
+++ app.js
@@ -0,0 +1,20 @@
+const process = require('process');
+const {argv}=process
+
+
+function calculadora(comando) {
+    
+
+switch (comando) {
+    case "sumar":
+        
+        break;
+    case "restar":
+        break;
+    case "multiplicar":
+        break
+    default:
+    case"dividir":
+        break;
+}
+}
